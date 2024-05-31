e562509dea9349ac2b181a71e5d0328a279e15fe
Aleksandr Pechak <alpechak@gmail.com>
2024-05-31 14:24:55 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,75 +1,60 @@
-// import { lookupService } from 'node:dns/promises';
-// import fs from 'node:fs/promises';
+/*
+------------------------- process() -------------------------
+*/
 
-/* ===Sync method=== */
+/*
+process.argv   -- аргументи запуску
+process.env    -- перелік всіх змінних оточення
+process.exit() -- завершення процесу NodeJS
+process.cwd()  -- поточна діректорія
+*/
 
-// const content = fs.readFileSync('./src/files/data.json');
-// fs.writeFileSync('./src/files/output.txt', content);
-
-// const entry = JSON.parse(content.toString());
-// const entry = content.toString();
-
-// console.log(entry);
-
-// console.log(content.toString());
-
-/* ===Callback method=== */
-
-// fs.readFile('./src/files/data.json', (err, content) => {
-//     fs.writeFile('./src/files/output.txt', content, (err) => {
-//         console.log('write');
-//     });
-//     console.log('read');
-// });
-// console.log('finish');
-
-/* ===Async await method=== */
-
-// const content = await fs.readFile('./src/files/data.json');
-// await fs.writeFile('./src/files/output.txt',content);
-
-// console.log(content.toString());
-
-/* ============================ */
-
-// import fs from 'node:fs/promises';
-
-// const filePath = './src/files/data.txt';
-
-// const fileOperation = async ({ action, data, path }) => {
-//   switch (action) {
-//     case 'read': {
-//       const result = await fs.readFile(filePath, 'utf8');
-//       console.log(JSON.parse(result).message);
-//       break;
-//     }
-//     case 'add': {
-//       const append = await fs.appendFile(filePath, data);
-//       console.log(append);
-//       break;
-//     }
-
-//     case 'replace': {
-//       const replace = await fs.writeFile(filePath, data);
-//       console.log(replace);
-//       break;
-//     }
-//     case 'rename': {
-//       //  fs.rename(filePath, `${filePath}smth`);
-//       fs.rename('text.txt', 'newtext.txt');
-//     }
-//     case 'delete': {
-//       fs.unlink(path);
-//     }
-
-//     default:
-//       console.log('Unknown action');
-//       break;
-//   }
-// };
-
-// fileOperation({ action: 'read' });
-// fileOperation({ action: 'add', data: '\n Aleksandr' });
-// fileOperation({ action: 'replace', data: 'New data'});
-// fileOperation({ action: 'rename'});
-// fileOperation({ action: 'delete', path: './src/files/data.txtsmth' });
+process.on('uncaughtException', (err) => {
+        console.error('SMTH HAPPEND! HELP: ', err.code);
+        process.exit(1);
+      });
+      
+      process.on('warning', (warning) => {
+        console.warn(warning.name);
+        console.warn(warning.message);
+        console.warn(warning.stack);
+      });
+      
+      /*
+      ------------------------- booksController -------------------------
+      */
+      
+      import { booksController } from './controllers/index.js';
+      
+      const logAsyncResult = async (cb) => console.log(await cb);
+      
+      const invokeAction = async ({ action, id, title, author }) => {
+        switch (action) {
+          case 'getAll': {
+            logAsyncResult(booksController.getAll());
+            break;
+          }
+          case 'getById': {
+            logAsyncResult(booksController.getById(id));
+            break;
+          }
+          case 'add': {
+            logAsyncResult(booksController.add({ title, author }));
+            break;
+          }
+          case 'update': {
+            logAsyncResult(booksController.update(id, { title, author}));
+            break;
+          }
+          case 'remove': {
+            logAsyncResult(booksController.remove(id));
+            break;
+          }
+          case 'default': {
+            console.log('Unknown action');
+            break;
+          }
+        }
+      };
+      
+      invokeAction({action: 'getAll'});
